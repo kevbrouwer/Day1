0ac1fecaeccdeead3e32fcb6ef2d7b5dd94e50a8
Kevin <kevbrouwer@gmail.com>
2018-08-27 05:43:19 -0600

    Day 2, PArt day 1

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Open cmd
 locate project folder
 use `cd` and `dir` to navigate to project folder
 run python program using python file name
- 
+ # `cd` is used to open a folder and `dir` is used to look inside a folder
+ How to run a script in atom using 'ctrl + shit b' and how to search for something using 'ctrl shift P'
--- day1.py
+++ day1.py
@@ -1,7 +1,28 @@
 print(1234)
-print("All Good")
+print("All GooF "*3 )
 a=10
 b=2
 c=a+b
 print(c)
 print(c+c)
+message = "Good day to learn something new"
+print(message)
+print(len(message))
+print(message[5])
+print(message[0:6])
+print(message[:5])
+print(message[6:30])
+print(message.lower())
+print(message.count('o'))
+print(message.find('w'))
+message = message.replace('Good','Best')
+print(message)
+name='Kevin'
+messag_to_me = message + ', ' + name
+print(messag_to_me)
+messag_to_me = '{}, {}, {}'.format(name,message,name)
+print(messag_to_me)
+me_me =f'{name.upper()},{name}.Ja'
+print(me_me)
+print(dir(name))
+print(help(str))
